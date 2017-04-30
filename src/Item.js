efdcbb47d393af16fa6f35f44a100222f5f1dc03
RossiIlieva <rossi_tiru@yahoo.com>
2017-04-30 16:27:47 -0500

    added item component

---
+++ src/Item.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  render() {
+    return (
+      <div className="Item">
+        <input type="checkbox" id="check-one"/>Milk
+        
+      </div>
+    );
+  }
+}
+
+export default Item;
