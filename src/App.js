4664c051f97a351a5b7821e39d95432aa739a898
RossiIlieva <rossi_tiru@yahoo.com>
2017-04-30 16:48:59 -0500

    improving item component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,27 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Shopping</h2>
         </div>
-        <Item />
+
+        <div className="form">
+        <Item>Milk</Item>
+        <Item>Eggs</Item>
+        <Item>Bread</Item>
+        <Item>Water</Item>
+        </div>
+
+
+        <div id="buttons" className="row">
+            <div className="col-xs-6">
+              <button className="btn btn-primary" id="button-add">Add</button>
+            </div>
+            <div className="col-xs-6">
+
+              <button className="btn btn-primary" id="button-delete">Delete</button>
+            </div>
+        </div>
+
       </div>
+
     );
   }
 }
--- src/Item.js
+++ src/Item.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 
 class Item extends Component {
   render() {
+
     return (
       <div className="Item">
-        <input type="checkbox" id="check-one"/>Milk
-        
+        <input type="checkbox" id="check-one"/>
+        {this.props.children}
       </div>
     );
   }
