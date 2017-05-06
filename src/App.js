e209f970bf324cb8155ea2a56c9b8a2976227129
RossiIlieva <rossi_tiru@yahoo.com>
2017-05-06 17:58:26 -0500

    fixed warning messages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import Item from './Item.js';
 import Btns from './Btns.js';
-import "./styles.css";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-        <h1 class="jumbotron text-center">Shopping</h1>
+        <h1 className="jumbotron text-center">Shopping</h1>
 
 
         </div>
